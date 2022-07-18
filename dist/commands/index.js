34bcf780be63814ccabd466f795053e9051d6eca
Lucas Leadbetter <lucas@apollographql.com>
2022-07-18 10:09:15 -0400

    package update

--- dist/commands/index.js
+++ dist/commands/index.js
@@ -1,15 +1,8 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.TestCommand = void 0;
-var test_1 = require('./test');
-Object.defineProperty(exports, 'TestCommand', {
-  enumerable: true,
-  get: function () {
-    return __importDefault(test_1).default;
-  },
-});
+var test_1 = require("./test");
+Object.defineProperty(exports, "TestCommand", { enumerable: true, get: function () { return __importDefault(test_1).default; } });
--- dist/graphql/client.js
+++ dist/graphql/client.js
@@ -1,24 +1,14 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.getTestClient = exports.getApolloClient = void 0;
-const graphql_request_1 = require('graphql-request');
-const getApolloClient = (key, { useSudo }) => {
-  return new graphql_request_1.GraphQLClient(
-    'https://api.apollographql.com/graphql',
-    {
-      headers: Object.assign(
-        { 'x-api-key': key },
-        useSudo ? { 'apollo-sudo': 'true' } : {},
-      ),
-    },
-  );
-};
+const graphql_request_1 = require("graphql-request");
+const getApolloClient = (key, { useSudo }) => new graphql_request_1.GraphQLClient('https://api.apollographql.com/graphql', {
+    headers: Object.assign({ 'x-api-key': key }, (useSudo ? { 'apollo-sudo': 'true' } : {})),
+});
 exports.getApolloClient = getApolloClient;
-const getTestClient = (endpoint) => {
-  return new graphql_request_1.GraphQLClient(endpoint, {
+const getTestClient = (endpoint) => new graphql_request_1.GraphQLClient(endpoint, {
     headers: {
-      'user-agent': 'x-apollo-test-util',
+        'user-agent': 'x-apollo-test-util',
     },
-  });
-};
+});
 exports.getTestClient = getTestClient;
